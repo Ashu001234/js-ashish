48594619247f4b1535bba14fcbe92311bb7ddd55
Ashish Girpunje <114429275+Ashu001234@users.noreply.github.com>
2023-10-04 08:43:04 +0530

    Add files via upload

---
+++ math_and_number.js
@@ -0,0 +1,34 @@
+
+// ******************** numbers***************
+/*     let score =100
+       console.log(score);
+
+
+const balance = new Number(100)
+console.log(balance); 
+console.log(balance.toString());
+console.log(balance.toFixed(2));
+
+
+      const myNumber =22.45
+  console.log(myNumber.toPrecision(5));
+      const hisNumber =2222222
+
+      console.log(hisNumber.toLocaleString('en-IN'));
+      console.log(hisNumber.toLocaleString());
+
+   */
+// ------------------- Maths -----------------
+/*
+         console.log(Math);
+       console.log(Math.abs(-10));
+       console.log(Math.round(4.3));
+       console.log(Math.round(4.7));
+       console.log(Math.ceil(4.2));
+       console.log(Math.floor(4.7));
+       console.log(Math.max(1,2,3,6));
+       console.log(Math.min(1,2,3,6));
+       console.log(Math.sqrt(25));
+       console.log(Math.tan(30));
+       console.log(Math.pow(2,3));
+*/
