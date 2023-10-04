790f23149d5bc4b873c3ab69088e5c5b52867b6f
Ashish Girpunje <114429275+Ashu001234@users.noreply.github.com>
2023-10-04 08:45:53 +0530

    Add files via upload

---
+++ datatype.js
@@ -0,0 +1,175 @@
+  "use strict"; // it is use for newer version
+
+
+
+    //----------------- Types of data type-------------------
+
+  
+// *************************** primitive Data types********************
+    /*
+     1. number
+     
+     if number is biger you can use BigInt
+     let age=18;
+     console.log(age)
+     */
+    /* 
+    2. string 
+    let name ="ashish"
+     console.log(name);
+     */
+    /* 3. bo0lean
+     let isLoginIn =true
+     */
+    /*4. nul 
+     completely empty
+    const outSideTemp =null
+    */
+    /*5.  undefined =variable is declare  memmory space declare but value not decided
+    let userEmail;
+    
+    */
+    /*  6  symbol =  use for unique value 
+     const id = Symbol('123')
+      const anotherId =Symbol('123')
+       console.log(id== anotherId);
+ */
+   
+    /*  72BigInt =   use for large value or Scientific value */
+
+
+
+    // **************** non primitive datatype********************
+    //  It is aslo known as  refrence type
+    /*
+    1.object    
+      
+      let myObj ={
+      name:"ashish"
+      age:23,
+     }
+     */
+    /*
+     2. Array
+    [   " Ashish", " Pratik", " Paras"];
+    */
+    /*3. function
+
+    const myFunction = function(){
+
+      console.log(" hello world");
+      
+    }
+    console.log(typeof myFunction);
+    */
+
+// --------------  conversion  ----------------------
+/*
+  let score =33
+
+  console.log (typeof score);
+ console.log (typeof (score));
+  */
+
+
+ /* let score = true
+    console.log( typeof score);
+    console.log( typeof (score));
+
+    let valueInNumber = Number(score)
+    console.log( typeof valueInNumber);
+  console.log(valueInNumber);
+*/
+//    "33"= 33
+// "33abc" =NaN
+//  true =1
+//  false =0
+/*
+let isLoggedIn =0
+ let booleanIsLoggedIn =Boolean(isLoggedIn)
+ console.log(booleanIsLoggedIn);
+ */
+//  "" =false
+// "hitesh" = true
+
+ /*
+let inNumber =33
+
+  let stringInNumber= String(inNumber)
+   console.log(stringInNumber);
+   console.log( typeof stringInNumber);
+*/
+
+
+// -----------------operations----------------
+
+/*
+ let value =3
+  let negValue = -value 
+  console.log(negValue);
+  */
+
+  /*
+  console.log(2+2);
+  console.log(2-2);
+  console.log(2/2);
+  console.log(2*2);
+  console.log(2**2);
+  console.log(2**3);
+   console.log(2%3);
+  */ 
+
+/*
+    let str1="hi"
+     let str2 =" pooja"
+      let str3= str1 + str2
+      console.log(str3);
+      */
+
+   /*
+        console.log(1 +"2");
+        console.log("1"+2);
+        console.log("1"+2+2);
+        console.log(1 +2 +"2");
+        console.log((3 + 4)* 5 % 3);
+        */
+
+/*
+      let proGame=100
+      // proGame++
+       ++proGame
+      console.log(proGame);
+      */
+
+      // ------------------- comparison--------------------------------
+       
+      /*
+      console.log( 2 > 1);
+       console.log( 2 < 1);
+       console.log( 2 == 1);
+       console.log( 2 != 1);
+       console.log( 2  >=  1);
+  
+      */
+
+      //  console.log( "2" > 1);
+      //  console.log( "02" > 1);
+
+
+      // console.log(null > 0);
+      // console.log(null == 0);
+      // console.log( null >= 0);
+
+
+
+      // console.log( undefined > 0);
+      // console.log(undefined == 0);
+      // console.log( undefined >= 0);
+
+
+      // === it check strictly using datatype 
+
+          
+            // console.log("2"=== 2);
+
+             
