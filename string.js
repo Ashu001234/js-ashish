7673c12131d27744fd6adce061ebc8a57b989586
Ashish Girpunje <114429275+Ashu001234@users.noreply.github.com>
2023-10-04 08:37:33 +0530

    Add files via upload

---
+++ string.js
@@ -0,0 +1,33 @@
+ /*
+ 
+   const name= "ashish"
+   const sirName ="girpunje"
+  console.log(` hello my name is ${name} rajesh ${sirName}`);
+ */
+
+/*
+   const gameName = new String ('ash-ish-rg')
+   console.log(gameName [0]);
+   console.log(gameName.__proto__); 
+   console.log(gameName.length);
+   console.log(gameName.toUpperCase());
+   console.log(gameName.charAt(5)); 
+   console.log(gameName.indexOf('h'));
+   const newString =gameName .substring(0,4)
+   console.log(newString);
+    const anotherString = gameName.slice(-8,4)
+    console.log(anotherString);
+*/
+/*
+       const newStringOne ="     ashish   "
+       console.log(newStringOne);
+       console.log(newStringOne.trim());
+ */
+
+  /*
+       const url = "https://hitesh.com/hithesh%20choudary"
+       console.log(url.replace('%20','-'));
+       console.log(url.includes('ashu'));
+   */
+
+      //   console.log(gameName.split('-'));
