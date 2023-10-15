b0c55695e92383133672163538902b083a08a183
Ashish Girpunje <114429275+Ashu001234@users.noreply.github.com>
2023-10-15 08:24:11 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,76 @@
+ 
+
+   const myArray =[0,1,2,3,4,5]
+//   console.log(myArray[0]);
+
+  const hiArray = new Array(1,2,3,4)
+//   console.log(hiArray[2]);
+
+/*
+ myArray.push(6)
+ console.log(myArray);
+ myArray.pop()55
+ console.log(myArray);
+ */
+
+
+ /*
+ myArray.unshift(6)
+ console.log(myArray);
+ myArray.shift()
+console.log(myArray);
+*/
+
+
+// const newArray = myArray.join()
+// console.log(newArray)
+// console.log( typeof newArray)
+ 
+/*
+console.log("A",myArray);
+const myAr1 =myArray.slice(1,3)
+console.log(myAr1);
+console.log("B ", myArray);
+const myAr2 = myArray.splice(1,3)
+console.log(myAr2);
+console.log("c ",myArray);
+*/
+
+
+// -------------------------------------------------------------------------
+/*
+
+    const  diplomaBuddies =["Pratik","Vishal","Sudhanshu"]
+             // console.log(diplomaBuddies[2]);
+    const degreeBuddies =["Sagar","Swapnil","Siddhesh"]
+    diplomaBuddies.push(degreeBuddies)
+               console.log(diplomaBuddies);
+
+
+//  allBuddies = diplomaBuddies.concat(degreeBuddies)
+//  console.log(allBuddies);
+
+// myBuddies=[...diplomaBuddies,...degreeBuddies]
+// console.log(myBuddies);
+
+/*
+// const another_array =[1,2,3,[4,5,6],7,[8,9,[10]]]
+// console.log(another_array);
+// const new_another_array =another_array.flat(Infinity)
+// console.log(new_another_array);
+
+*/
+
+// console.log(Array.isArray("ashish"));
+// console.log(Array.from("ashish"));
+// console.log(Array.from({name:"ashish"}));
+
+/*
+let num1 =100
+let num2=200
+let num3 =300
+ console.log(Array.of(num1,num2,num3));
+ */
+
+
+
